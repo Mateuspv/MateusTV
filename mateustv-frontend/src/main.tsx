28d6f23e78ae0ba4516b52a4450e3f9fa6e4c038
Mateuspv <mateuspv2003@gmail.com>
2024-11-17 16:16:27 -0300

    Estruturacao

--- mateustv-frontend/src/main.tsx
+++ mateustv-frontend/src/main.tsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { createTheme, CssBaseline, ThemeProvider, } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary:{
+      main: '#ffffff',
+    }
+  }
+})
+
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <ThemeProvider theme={ darkTheme}>
+      <CssBaseline/>
+      <BrowserRouter>
+      <App />
+      </BrowserRouter>
+
+    </ThemeProvider>
+  </React.StrictMode>
 )
