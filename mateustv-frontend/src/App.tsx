e340ab060e7ee70818d0cb46b070404683e472a9
Mateuspv <mateuspv2003@gmail.com>
2024-11-18 00:40:28 -0300

    Header

--- mateustv-frontend/src/App.tsx
+++ mateustv-frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import { Route, Routes } from "react-router-dom"
-import Homepage from "./app/pages/home"
+//import { Route, Routes } from "react-router-dom"
+//import Homepage from "./app/pages/home"
 import Header from "./app/components/Header"
 import Footer from "./app/components/Footer"
 
@@ -8,9 +8,9 @@ function App() {
   return (
     <div className="wrapper">
       <Header />
-      <Routes>
+      {/* <Routes>
         <Route path="/:id?" element={<Homepage />} />
-      </Routes>
+      </Routes> */}
       <Footer />
     </div>
   )
--- mateustv-frontend/src/app/components/Header/index.tsx
+++ mateustv-frontend/src/app/components/Header/index.tsx
@@ -1,5 +1,72 @@
-function Header() {
+import { AppBar, Box, Container, InputAdornment, TextField, Toolbar } from "@mui/material";
+import DensityMediumIcon from '@mui/icons-material/DensityMedium';
+import SearchIcon from '@mui/icons-material/Search';
 
+function Header() {
+    return (
+        <AppBar>
+            <Toolbar>
+                <Box display="flex">
+                    <DensityMediumIcon />
+                </Box>
+                <Box>
+                    <img src="/assets/fix/MateusTV.png"
+                        alt="MateusTV"
+                        style={{
+                            height: 40,
+                            alignItems: "center",
+                            justifyContent: "center",
+                            display: "flex"
+                        }}
+                    />
+                </Box>
+
+                <Container sx={{ display: 'flex', justifyContent: 'center', paddingRight: '3rem' }}>
+                    <TextField
+                        variant="outlined"
+                        placeholder="Pesquisar"
+                        size="small"
+                        sx={{
+                            width: '35%',
+                            borderRadius: 1,
+                            '& .MuiOutlinedInput-root': {
+                                '& fieldset': {
+                                    borderColor: 'Gray',
+                                },
+                                '&:hover fieldset': {
+                                    borderColor: '#a0a0a0',
+                                },
+                                '&.Mui-focused fieldset': {
+                                    borderColor: '#9147ff',
+                                },
+                            },
+                        }}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <SearchIcon/>
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                </Container>
+            
+                <Box sx={{justifyContent: 'left'}}>
+                <img src="/assets/fix/UserIcon.png"
+                        alt="UserIcon"
+                        style={{
+                            height: 40,
+                            alignItems: "center",
+                            justifyContent: "center",
+                            display: "flex",
+                            borderRadius: '50%'
+                        }}
+                    />
+                </Box>
+
+            </Toolbar>
+        </AppBar>
+    )
 }
 
 export default Header;
